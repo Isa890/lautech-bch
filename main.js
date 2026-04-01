2b42b74d0eafdd23d0cbdc087d51d50e36663a00
isaac Babalola <isaacbabalola06@gmail.com>
2026-04-01 10:23:54 -0700

    f1

---
+++ main.js
@@ -0,0 +1,12 @@
+const currentLevel = document.querySelector('.current-level');
+
+function currentLevel(){
+  if (currentLevel < 100 && currentLevel >=500){
+    console.log('valid');
+    else{
+      console.log('invalid');
+    }
+  } if (currentLevel = 100){
+    var currentLevel = link.href = '100.html';
+  }
+}
